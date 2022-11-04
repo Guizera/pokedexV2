bb86ba5e9ef28769c80150ef52da25d0b09a7d6b
Guilherme Cunha <guizera7@gmail.com>
2022-11-04 17:28:15 -0300

    Initial Commit

---
+++ pokedexV2/pokedexV2App.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexV2App.swift
+//  pokedexV2
+//
+//  Created by José Alves da Cunha on 04/11/22.
+//
+
+import SwiftUI
+
+@main
+struct pokedexV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
